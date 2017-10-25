b933bed00870fc3ad8a100a9a8b144df1a97673e
Bruno Ribeiro Passos <brurpassos@gmail.com>
2017-10-24 23:58:26 -0200

    Webpack configured

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
+import "../css/main.scss"
 import { teste } from "./test";
+
 
 console.log("noix");
 console.log(teste);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+  filename: 'main.css'
+});
 
 module.exports = {
   entry: './src/js/app.js',
@@ -7,4 +13,28 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/dist'
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    extractPlugin
+  ]
 };
