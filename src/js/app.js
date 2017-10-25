d032084384d4e5de2778af5edbbbd9b809a48edc
Bruno Ribeiro Passos <brurpassos@gmail.com>
2017-10-25 16:42:21 -0200

    Dynamic form start

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 import "../css/main.scss"
-import { teste } from "./test";
+import { formData } from "./data";
 
 
-console.log("noix");
-console.log(teste);
+debugger;
+var form = document.createElement("form");
+document.getElementById("app").append(form);
+
+for(var i = 0; i < formData.length; i++) {
+  var formElement = document.createElement("input");
+  debugger;
+  formElement.id = formData[i].id;
+  formElement.name = formData[i].name;
+  formElement.placeholder = formData[i].placeholder;
+  formElement.type = formData[i].type;
+  form.appendChild(formElement);
+}
