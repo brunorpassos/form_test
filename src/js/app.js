6b084f58505453800c6b242e71d7ac86c233c769
Bruno Ribeiro Passos <brurpassos@gmail.com>
2017-10-26 19:41:10 -0200

    Image Upload Saga

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,23 @@ document.getElementById("app").append(form);
 
 for(var i = 0; i < formData.length; i++) {
   var formElement = document.createElement("div");
-  formElement.className = "form-element";
+  switch(formData[i].type) {
+    case "file":
+      formElement.className = "form-file-input";
+      createFormFileUpload(formElement, formData);
+      break;
+    case "submit":
+      formElement.className = "form-element";
+      createFormSubmit(formElement, formData);
+      break;
+    default:
+      formElement.className = "form-element";
+      createFormInput(formElement, formData);
+      break;  
+  }
+}
 
+function createFormInput(formElement, formData) {
   var formLabel = document.createElement("label");
   formLabel.htmlFor = formData[i].id;
   formLabel.append(formData[i].placeholder);
@@ -23,6 +38,45 @@ for(var i = 0; i < formData.length; i++) {
   form.appendChild(formElement);
   formElement.appendChild(formLabel);
   formElement.appendChild(formInput);
+  setFocusEventListener(formInput.id);
+}
+
+function createFormSubmit(formElement, formData) {
+  var formSubmit = document.createElement("button");
+  form.appendChild(formElement);
+  formSubmit.className = "form-submit";
+  formSubmit.setAttribute("disabled", true);
+  formSubmit.append(formData[i].placeholder);
+  formElement.appendChild(formSubmit);
+}
+
+function createFormFileUpload(formElement, formData) {
+  var imageUploadIcon = document.createElement("img");
+  imageUploadIcon.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Ei-share-apple.svg/2000px-Ei-share-apple.svg.png";
+  imageUploadIcon.style.width = "50px";
+  imageUploadIcon.style.height = "50px";
+
+  var formImageUpload = document.createElement("label");
+  formImageUpload.htmlFor = formData[i].id;
+  formImageUpload.className = "form-image-upload";
+  formImageUpload.append(imageUploadIcon);
+
+
+  var formInput = document.createElement("input");
+  formInput.id = formData[i].id;
+  formInput.name = formData[i].name;
+  formInput.type = formData[i].type;
+  formInput.style.display = "none";
+
+  var formImageThumb = document.createElement("img");
+  formImageThumb.src = "https://skinsjar.com/img/stats/profile.svg";
+  formImageThumb.style.width = "100px";
+  formImageThumb.style.height = "100px";
+
+  form.appendChild(formElement);
+  formElement.appendChild(formImageUpload);
+  formElement.appendChild(formInput);
+  formElement.appendChild(formImageThumb);
   setFocusEventListener(formInput.id);
 }
 
