a42adf19683c0a08b97fe89edfe10608a6d4e8c9
Bruno Ribeiro Passos <brurpassos@gmail.com>
2017-10-26 16:36:20 -0200

    Layout structured

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,39 @@
 import "../css/main.scss"
 import { formData } from "./data";
 
-
-debugger;
 var form = document.createElement("form");
+form.className = "customer-form"
 document.getElementById("app").append(form);
 
 for(var i = 0; i < formData.length; i++) {
-  var formElement = document.createElement("input");
-  debugger;
-  formElement.id = formData[i].id;
-  formElement.name = formData[i].name;
-  formElement.placeholder = formData[i].placeholder;
-  formElement.type = formData[i].type;
+  var formElement = document.createElement("div");
+  formElement.className = "form-element";
+
+  var formLabel = document.createElement("label");
+  formLabel.htmlFor = formData[i].id;
+  formLabel.append(formData[i].placeholder);
+  formLabel.className = "form-label";
+
+  var formInput = document.createElement("input");
+  formInput.className = "form-input";
+  formInput.id = formData[i].id;
+  formInput.name = formData[i].name;
+  formInput.type = formData[i].type;
+
   form.appendChild(formElement);
+  formElement.appendChild(formLabel);
+  formElement.appendChild(formInput);
+  setFocusEventListener(formInput.id);
+}
+
+function setFocusEventListener(inputId) {
+  document.getElementById(inputId).addEventListener("focus", function() {
+    this.previousSibling.classList.add("active-label")
+  });
+
+  document.getElementById(inputId).addEventListener("focusout", function() {
+    if (!this.value.trim()) {
+      this.previousSibling.classList.remove("active-label");
+    } 
+  });
 }
